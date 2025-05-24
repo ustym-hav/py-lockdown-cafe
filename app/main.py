a6523af88e41d5e129a71d952a21d3e530fc1759
Ustym <Ustym.hav@gmail.com>
2025-05-24 19:08:12 +0300

    vaccine_time

--- app/main.py
+++ app/main.py
@@ -7,13 +7,16 @@ from app.cafe import Cafe
 
 def go_to_cafe(friends: List[dict], cafe: Cafe) -> str:
     masks_to_buy = 0
+    check_vaccine = None
     for friend in friends:
         try:
             cafe.visit_cafe(friend)
         except VaccineError:
-            return "All friends should be vaccinated"
+            check_vaccine = True
         except NotWearingMaskError:
             masks_to_buy += 1
+    if check_vaccine:
+        return "All friends should be vaccinated"
     if masks_to_buy > 0:
         return f"Friends should buy {masks_to_buy} masks"
     return f"Friends can go to {cafe.name}"
